fab49b8a8ece699934e04dfb46d0cc35b4eae06a
monosoul <Kloz.Klaud@gmail.com>
2022-08-29 11:32:23 +0200

    Minimize reproducer

--- src/main/kotlin/dev/monosoul/mockkissuereproducer/ValueClassWithValidationInConstructor.kt
+++
@@ -1,8 +0,0 @@
-package dev.monosoul.mockkissuereproducer
-
-@JvmInline
-value class ValueClassWithValidationInConstructor(val value: Int) {
-    init {
-        require(value in 1..10) { "${value} is out of bounds" }
-    }
-}
