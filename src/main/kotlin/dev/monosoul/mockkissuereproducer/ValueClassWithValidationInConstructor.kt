b9f7d559487bf63bbcb509fe94890fb9085854b2
monosoul <Kloz.Klaud@gmail.com>
2022-08-29 11:28:17 +0200

    Use a more realistic scenario

--- src/main/kotlin/dev/monosoul/mockkissuereproducer/ValueClassWithValidationInConstructor.kt
+++ src/main/kotlin/dev/monosoul/mockkissuereproducer/ValueClassWithValidationInConstructor.kt
@@ -3,6 +3,6 @@ package dev.monosoul.mockkissuereproducer
 @JvmInline
 value class ValueClassWithValidationInConstructor(val value: Int) {
     init {
-        require(false) { "Validation failed" }
+        require(value in 1..10) { "${value} is out of bounds" }
     }
 }
